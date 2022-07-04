5775adebdca21598170c7650b2302739c4d748f8
Yago Henrique <yago.devs@gmail.com>
2022-07-04 10:33:13 -0300

    Configurações movidas de eslintrc para index

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-const eslintrc = require('./.eslintrc')
+const prettierConfig = require('@yago.gif/prettier-config')
 
-module.exports = eslintrc;
+module.exports ={
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+    es2021: true
+  },
+  extends: [
+    "airbnb",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    'prettier/prettier': [
+      "error", prettierConfig
+    ],
+    'space-before-function-paren': "off",
+    'react/prop-types': "off",
+    'react/jsx-filename-extension': [
+      "error",
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ],
+    'import/prefer-default-export': "off",
+    'import/no-cycle': "off",
+    'comma-dangle': "off",
+    'object-curly-newline': "off",
+    'no-console': "off",
+    'arrow-parens': [2, "as-needed"],
+    'react/jsx-props-no-spreading': "off",
+    'prefer-destructuring': "off"
+  }
+}
